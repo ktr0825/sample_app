49301fef8825152ed4003c2d4750a8c335f953b0
ktr5280 <jascine@icloud.com>
2018-05-16 16:45:05 +0900

    Make Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  resources :users
 end
